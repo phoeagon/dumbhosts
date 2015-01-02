4e46a43913392c94dbcf137935cf1c0b0fbafb8c
phoeagon <phoeagon@gmail.com>
2015-01-03 07:32:08 +0800

    fixes name

--- generate.sh
+++ generate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 wget https://raw.github.com/smarthosts/SmartHosts/master/trunk/hosts -O hosts.ori
-grep -v localhost hosts.ori | sed -e 's/[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+/0.0.0.0/g' >hosts.tmp
+grep -v localhost hosts.ori | sed -e 's/[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+/0.0.0.0/g' | sed -e 's/SmartHosts/DumbHosts/g' >hosts.tmp
 awk 'NR==1{print "127.0.0.1  localhost"}7' hosts.tmp >hosts
 rm hosts.tmp
